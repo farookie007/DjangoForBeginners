97972e634efe3d24df1fd7bae2c281d880ef1ed0
Farouk Adegboyega <faroukadekunle001@gmail.com>
2022-08-28 12:21:05 +0100

    Created the Post model, updated the admin site and migrate

--- ch_4/posts/models.py
+++ ch_4/posts/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        n = 20                              # cut-off value for the text displayed
+        return f"{self.text[:n] + '...' if len(self.text)>n else self.text}"
