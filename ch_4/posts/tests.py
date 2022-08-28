db027bc3766544360a76ff0e75ab6500a49f60a4
Farouk Adegboyega <faroukadekunle001@gmail.com>
2022-08-28 17:02:06 +0100

    Updated the tests for the app and made the code more DRY

--- ch_4/posts/tests.py
+++ ch_4/posts/tests.py
@@ -1,7 +1,8 @@
-from django.test import TestCase, SimpleTestCase
+from django.test import TestCase
 from django.urls import reverse
 
 from .models import Post
+
 
 # Create your tests here.
 
@@ -22,23 +23,56 @@ class PostModelTests(TestCase):
     def test_object_type(self):
         content = self.get_post_content()
         self.assertEqual(type(content), type(''))
-
+        
 
 class HomePageViewTests(TestCase):
-    def test_url_exists(self):
-        response = self.client.get(reverse('home'))
-        self.assertEqual(response.status_code, 200)
-    
+    app_name = None
+    view_name = 'home'
+    template_path = 'posts/home.html'
+    url_location = '/'
+
+    def _generate_reverse_url(self):
+        return f"{self.app_name + ':' if self.app_name else ''}{self.view_name}"
+
     def test_url_exists_by_name(self):
-        resp = self.client.get('/')
+        url = self._generate_reverse_url()
+        response = self.client.get(reverse(url))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_exists_by_location(self):
+        resp = self.client.get(self.url_location)
         self.assertEqual(resp.status_code, 200)
 
-
-class AboutPageViewTests(SimpleTestCase):
-    def test_url_exists(self):
-        response = self.client.get(reverse('about'))
-        self.assertEqual(response.status_code, 200)
-    
-    def test_url_exists_by_name(self):
-        resp = self.client.get('/about/')
+    def test_view_map_correct_template(self):
+        url = self._generate_reverse_url()
+        resp = self.client.get(reverse(url))
         self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, self.template_path)
+
+                
+    
+
+
+class AboutPageViewTests(TestCase):
+    app_name = None
+    view_name = 'about'
+    template_path = 'posts/about.html'
+    url_location = '/about/'
+
+    def _generate_reverse_url(self):
+        return f"{self.app_name + ':' if self.app_name else ''}{self.view_name}"
+
+    def test_url_exists_by_name(self):
+        url = self._generate_reverse_url()
+        response = self.client.get(reverse(url))
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_exists_by_location(self):
+        resp = self.client.get(self.url_location)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_map_correct_template(self):
+        url = self._generate_reverse_url()
+        resp = self.client.get(reverse(url))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, self.template_path)
