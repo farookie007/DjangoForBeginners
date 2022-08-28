fcc2d64693dac5b3c0d77c95d4a5b1964193af33
Farouk Adegboyega <faroukadekunle001@gmail.com>
2022-08-28 11:22:09 +0100

    Done with Chapter 3
    Created and ran tests for the views created
    Added the URLConf for the pages

--- ch_3/pages/tests.py
+++ ch_3/pages/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class HomeTests(SimpleTestCase):
+    def test_url_exists_at_current_location(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+
+class AboutTests(SimpleTestCase):
+    def test_url_exists_at_current_location(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
--- ch_3/pages/views.py
+++ ch_3/pages/views.py
@@ -3,4 +3,10 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 class HomeView(TemplateView):
+    # handles the Homepage view
     template_name = "pages/home.html"
+
+
+class AboutView(TemplateView):
+    # handles the About page view
+    template_name = "pages/about.html"
