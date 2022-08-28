6a3499dd1123f85f30e3270f8522ef149f802073
Farouk Adegboyega <faroukadekunle001@gmail.com>
2022-08-28 13:09:24 +0100

    Used the ListView class from the generic class-based views

--- ch_4/posts/views.py
+++ ch_4/posts/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+import imp
+from django.views.generic import ListView, TemplateView
+
+from .models import Post
 
 # Create your views here.
+class HomeView(ListView):
+    model = Post
+    template_name = 'posts/home.html'
+    context_object_name = 'posts_list'
+
+
+class AboutView(TemplateView):
+    template_name = 'posts/about.html'
