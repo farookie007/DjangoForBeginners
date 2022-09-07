038ab575eb3250f30688cc00a823e40932cafce6
Farouk Adegboyega <faroukadekunle001@gmail.com>
2022-09-07 19:39:19 +0100

    Initial commit for Chapter 7: User Accounts
    Created the 'accounts' APP to handle the signup functionlity and user creation
    Added the 'accounts' APP to the base project INSTALLED_APPS
    Added URLConf for the neccessary routes
    Created the view to handle user sign up functionality
    Created the 'signup.html' template

--- ch_5/blog/views.py
+++ ch_5/blog/views.py
@@ -24,10 +24,12 @@ class BlogCreateView(CreateView):
     template_name = 'blog/post_create.html'
     fields = ['title', 'author', 'body']
 
+
 class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'blog/post_update.html'
     fields = ['title', 'body']
+
 
 class BlogDeleteView(DeleteView):
     model = Post
--- ch_5/ch_5/settings.py
+++ ch_5/ch_5/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     # custom created APPs
     "blog.apps.BlogConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -126,3 +127,6 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
