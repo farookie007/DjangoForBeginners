794a4cd58defa5d0b0185c52433480f1d5e91b49
Farouk Adegboyega <faroukadekunle001@gmail.com>
2022-09-10 21:36:02 +0100

    Initial commit for Chapter 15
    Modified the views for the `articles` APP handle authorizations using mixins

--- newspaper_project/articles/views.py
+++ newspaper_project/articles/views.py
@@ -1,7 +1,11 @@
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    UserPassesTestMixin,
+)
 from django.views.generic import ListView, CreateView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 
 from .models import Article
 
@@ -9,31 +13,48 @@ from .models import Article
 # Create your views here.
 
 
-class ArticleListView(ListView):
+class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
     template_name = "article_list.html"
     context_object_name = "article_list"
+    login_url = "login"
 
 
-class ArticleDetailView(DetailView):
+class ArticleDetailView(LoginRequiredMixin, DetailView):
     model = Article
     template_name = "article_detail.html"
-    context_object_name = 'article'
+    context_object_name = "article"
+    login_url = "login"
 
 
-class ArticleUpdateView(UpdateView):
+class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     fields = ("title", "body")
     template_name = "article_edit.html"
+    login_url = "login"
+
+    def test_func(self):
+        # returns True if the author of the article is the same as the current user
+        return self.get_object().author == self.request.user
 
 
-class ArticleDeleteView(DeleteView):
+class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Article
     template_name = "article_delete.html"
     success_url = reverse_lazy("article_list")
+    login_url = "login"
+
+    def test_func(self):
+        # returns True if the author of the article is the same as the current user
+        return self.get_object().author == self.request.user
 
 
-class ArticleCreateView(CreateView):
+class ArticleCreateView(LoginRequiredMixin, CreateView):
     model = Article
     template_name = "article_new.html"
-    fields = ('title', 'body', 'author')
+    fields = ("title", "body")
+    login_url = "login"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
